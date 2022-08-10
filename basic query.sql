7abaff4b19e3e052c3ec54f874de7f44572971be
huien09 <huien92@yahoo.com.sg>
2022-08-10 19:38:04 +0800

    Add files via upload

--- basic query.sql
+++ basic query.sql
@@ -1,2 +1,16 @@
 use `nodelogin`;
-select * from accounts;
+select * from useraccounts;
+CREATE TABLE IF NOT EXISTS `useraccounts` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `active` varchar(5) NOT NULL,
+  `admintag` varchar(5) NOT NULL
+) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `usergroups` (
+  `groupname` varchar(250) NOT NULL,
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB;
+INSERT INTO `useraccounts` (`username`, `password`, `email`, `active`, `admintag`) VALUES ('test3', 'test', 'test@test.com', '1', '0');
+select * from usergroups;
+delete from usergroups where groupname = 'test5';
--- create table query task.sql
+++ create table query task.sql
@@ -1,9 +1,37 @@
-select * from usergroups;
-delete from usergroups where groupname = 'test5';
+use `nodelogin`;
+CREATE TABLE IF NOT EXISTS `useraccounts` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `active` varchar(5) NOT NULL,
+  `admintag` varchar(5) NOT NULL
+) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS `usergroups` (
+  `groupname` varchar(250) NOT NULL,
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB;
+create table if not exists `application`(
+`App_Acronym` varchar(100) NOT NULL,
+`App_Description` longtext,
+`App_Rnumber` int,
+`App_startDate` date,
+`App_endDate` date,
+`App_permit_Create` varchar(100) NOT NULL,
+`App_permit_Open` varchar(100) NOT NULL,
+`App_permit_toDoList` varchar(100) NOT NULL,
+`App_permit_Doing` varchar(100) NOT NULL,
+`App_permit_Done` varchar(100) NOT NULL
+) ENGINE=InnoDB;
+create table if not exists `plan`(
+`Plan_MVP_name` varchar(100) NOT NULL,
+`Plan_startDate` date,
+`Plan_endDate` date,
+`Plan_app_Acronym` varchar(100) NOT NULL
+) ENGINE=InnoDB;
 create table if not exists `Task`(
 `Task_name` varchar(100) NOT NULL,
-`Task_description` varchar(100) NOT NULL,
-`Task_notes` varchar(100) NOT NULL,
+`Task_description` longtext,
+`Task_notes` longtext NOT NULL,
 `Task_id` varchar(200) NOT NULL,
 `Task_plan` varchar(200) NOT NULL,
 `Task_app_Acronym` varchar(200) NOT NULL,
